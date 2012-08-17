2328a3d58c6dd435d4a5dcf4e66971aab80e4d08
Danielle <danielle@danielleleong.com>
2012-08-17 16:04:46 -0700

    Changed from fruits to dinosaurs

--- keywords.php
+++ keywords.php
@@ -9,35 +9,60 @@
 include('Services/Twilio.php');
 
 /* ## Controller ## */
+function menu(){
+	$response = new Services_Twilio_Twiml();
+	$response->sms('Dino facts! Reply with one of the following: brachiosaurus, stegosaurus, iguanodon, trex, raptor, or brontosaurus.');
+	echo $response;
+}
+
 function index(){
 	$response = new Services_Twilio_Twiml();
-	$response->sms('Reply with one of the following keywords: <orange>, <pear>, <lychee>, <longan>.');
+	$response->sms('Sorry, but you didn\'t spell your dino right. Reply with one of the following: brachiosaurus,  stegosaurus, iguanodon, trex, raptor, or brontosaurus.');
 	echo $response;
 }
 
-function orange(){
+function brontosaurus(){
 	$response = new Services_Twilio_Twiml();
-	$response->sms('Orange. Citrus unshiu.');
+	$response->sms('Sorry, but brontosaurus is not a real dinosaur. It is actually known as Apatosaurus. :)');
 	echo $response;
 }
 
-function pear(){
+function brachiosaurus(){
 	$response = new Services_Twilio_Twiml();
-	$response->sms('Pear. Pyrus communis.');
+	$response->sms('Brachiosaurus is a large sauropod dino that had longer forelegs than back legs. Long dino is long.');
 	echo $response;
 }
 
-function lychee(){
+function stegosaurus(){
 	$response = new Services_Twilio_Twiml();
-	$response->sms('Lychee. Litchi chinensis.');
+	$response->sms('Stegosaurus are the most recognizable dinosaurs. Even more than T-rex. According to Wikipedia.');
 	echo $response;
 }
 
-function longan(){
+function iguanodon(){
 	$response = new Services_Twilio_Twiml();
-	$response->sms('Longan. Dimocarpus longan.');
+	$response->sms('Did you know Iguanodons walk on all four feet? Also, no one knows what their thumb spikes are for.');
 	echo $response;
 }
+
+function trex(){
+	$response = new Services_Twilio_Twiml();
+	$response->sms('Juvenile t-rexes may have been covered in feathers. It\'s weird.');
+	echo $response;
+}
+
+function raptor(){
+	$response = new Services_Twilio_Twiml();
+	$response->sms('A real velociraptor is about the size of a medium sized dog.');
+	echo $response;
+}
+
+function apatosaurus(){
+	$response = new Services_Twilio_Twiml();
+	$response->sms('Apatosaurus belongs to the genus Brontosaurus, which is scientifically redundant.');
+	echo $response;
+}
+
 
 /* Read the contents of the 'Body' field of the Request. */
 $body = $_REQUEST['Body'];
@@ -49,17 +74,29 @@ $result = strtolower($result);
 
 /* ##Router## */
 switch ($result) {
-    case 'orange':
-        orange();
+	case 'menu':
+        menu();
         break;
-    case 'pear':
-        pear();
+    case 'brachiosaurus':
+        brachiosaurus();
         break;
-    case 'lychee':
-        lychee();
+    case 'brontosaurus':
+        brontosaurus();
         break;
-    case 'longan':
-        longan();
+    case 'stegosaurus':
+        stegosaurus();
+        break;
+    case 'iguanodon':
+       	iguanodon();
+        break;
+    case 'trex':
+       	trex();
+        break;	
+    case 'raptor':
+       	raptor();
+        break;
+    case 'apatosaurus':
+       	apatosaurus();
         break;
     /* Add new routing logic above this line. */
     default:
